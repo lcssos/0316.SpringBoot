20e1ca73fadc74483da2e168a112c1d31834a3fc
lcssos <lcs@hh-medic.com>
2016-04-21 00:33:18 +0800

    application.yml

---
+++ src/main/java/cn/martin/boot/controller/HomeController.java
@@ -0,0 +1,34 @@
+package cn.martin.boot.controller;
+
+import cn.martin.boot.component.FooComponent;
+import cn.martin.boot.entity.Login;
+import cn.martin.boot.mapper.LoginMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by lcssos on 16/4/20.
+ */
+@RestController
+@Slf4j
+public class HomeController {
+
+    @Resource
+    private FooComponent fooComponent;
+    @Resource
+    private LoginMapper loginMapper;
+
+    @RequestMapping("/home")
+    public String home(){
+
+        log.info(fooComponent.getUsername());
+
+        Login login = loginMapper.selectByPrimaryKey(1000000L);
+        log.info(login.getName());
+
+        return "home";
+    }
+}
